607101bf4897bdea7f8c98a561bd3f4e479888fa
lmcnaboe <lmcnaboe25@gmail.com>
2024-02-28 12:14:34 -0500

    Created new login page with functions

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ login.addEventListener("click", () => {
     slider.classList.remove("moveslider");
     formSection.classList.remove("form-section-move");
 });
+
+var attempt = 3; 
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Formget" && password == "formget#123"){
+    //change this to match with something in database
+alert ("Login successfully");
+window.location = "success.html"; // Redirecting to other page.
+//also change this to be able to access all of the entries on this account
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
