c60f30135d8de8f150de8f8e6786b2c42a823199
Shayne Lada <shaynelada@gmail.com>
2024-03-18 10:12:35 -0400

    emotion buttons dissapear

--- js/index.js
+++ js/index.js
@@ -15,24 +15,28 @@ login.addEventListener("click", () => {
     formSection.classList.remove("form-section-move");
 });
 
-let emotion = document.querySelector(".emotion-button");
-
-emotion.addEventListener("click", () => {
-    emotion.classList.add("fade");
-});
-
-function fade(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1){
-            clearInterval(timer);
-            element.style.display = 'none';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 50);
+function fade() {
+    document.getElementById("highNegative").style.backgroundColor="green";
 }
+
+// let emotion = document.querySelector(".emotion-button");
+
+// emotion.addEventListener("click", () => {
+//     emotion.classList.add("fade");
+// });
+
+// function fade(element) {
+//     var op = 1;  // initial opacity
+//     var timer = setInterval(function () {
+//         if (op <= 0.1){
+//             clearInterval(timer);
+//             element.style.display = 'none';
+//         }
+//         element.style.opacity = op;
+//         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+//         op -= op * 0.1;
+//     }, 50);
+// }
 
 //let attempt = 3;
 
