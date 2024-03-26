510a156bea71487fb06b559b04946e57f14aa0cf
Shayne Lada <shaynelada@gmail.com>
2024-03-26 10:13:11 -0400

    store design

--- js/index.js
+++ js/index.js
@@ -109,3 +109,20 @@ $('.form').find('input, textarea').on('keyup blur focus', function (e) {
     $(target).fadeIn(600);
     
   });
+
+  const productContainers = [...document.querySelectorAll('.product-container')];
+  const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+  const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+  productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+    
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+  })
