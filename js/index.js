334358906e963908c5dc66486761a3e7e3fce110
lmcnaboe <lmcnaboe25@gmail.com>
2024-03-27 11:12:42 -0400

    combined

--- js/index.js
+++ js/index.js
@@ -61,3 +61,13 @@ function fade(element) {
         item.scrollLeft -= containerWidth;
     })
   })
+
+  function showSection(sectionId) {
+    // Hide all sections
+    document.querySelectorAll('.section').forEach(section => {
+        section.style.display = 'none';
+    });
+
+    // Show the selected section
+    document.getElementById(sectionId).style.display = 'block';
+}
