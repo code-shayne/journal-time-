dc0a3c5aac611fb5f70ac1abee7d9ce653f03890
ava-mcnabb <avagmcnabb@gmail.com>
2024-04-10 05:02:49 -0400

    added entry table

--- index.sql
+++ index.sql
@@ -4,9 +4,20 @@ CREATE TABLE users (
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     salt VARCHAR(255) NOT NULL,
-    full_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE entries (
+    entry_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    emotion VARCHAR(255) NOT NULL,
+    entry_title VARCHAR(255) NOT NULL,
+    entry_body VARCHAR(255) NOT NULL,
+    entry_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 
 -- Function to generate a random salt
 DELIMITER //
@@ -28,5 +39,8 @@ DELIMITER ;
 -- Example registration query
 -- Replace 'input_username', 'input_email', and 'input_password' with actual user input
 SET @salt = generate_salt(32);
-INSERT INTO users (username, email, password_hash, salt, full_name)
+INSERT INTO users (username, email, password_hash, salt, first_name, last_name)
 VALUES ('input_username', 'input_email@example.com', SHA2(CONCAT('input_password', @salt), 256), @salt, 'Full Name');
+
+INSERT INTO entries (user_id, emotion, entry_title, entry_body)
+VALUES ('userid', 'emotion', 'entry_title', 'entry_body');
