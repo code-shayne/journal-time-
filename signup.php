ebe138198d4a6371b565a388af683c7734f9027e
ava-mcnabb <avagmcnabb@gmail.com>
2024-06-05 08:59:24 -0400

    login and sign up php

---
+++ signup.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $conn = mysqli_connect("localhost", "root", "Pineapple8656!", "journaltime");
+  
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $sql = "INSERT INTO users (username, email, password_hash) VALUES ('$username', '$email', '$password')";
+
+    if (mysqli_query($conn, $sql)) {
+      echo "Data saved successfully!";
+    } 
+    else {
+      echo "Error: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+  }
